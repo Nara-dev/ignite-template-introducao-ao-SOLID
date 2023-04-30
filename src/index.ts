e1983e929bd76d6c48693497dbb9d09c01ea6217
Ianara-cs <ianarasilva333@gmail.com>
2023-04-29 21:45:41 -0300

    documentação

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,12 @@
 import express from "express";
+import swagger from "swagger-ui-express";
 
 import { usersRoutes } from "./routes/users.routes";
+import swaggerDocument from "./swagger.json";
 
 const app = express();
+
+app.use("/api-docs", swagger.serve, swagger.setup(swaggerDocument));
 
 app.use(express.json());
 
